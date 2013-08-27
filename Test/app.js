6880d00646f28cd5b409ae26bd9e47bcecc78e09
Quang Huynh <quang.nguyenbh@gmail.com>
2013-08-27 09:48:24 +0700

    aaa

--- Test/app.js
+++ Test/app.js
@@ -5,10 +5,10 @@
 var app = angular.module("click", []);
 app.service("data", function () {
     return new click.ClientDataService();
-}).controller('createWebsiteCtrl', click.CreateWebsiteCtrl).controller('websiteListCtrl', click.WebsiteListCtrl.prototype.injection()).controller('websiteDetailCtrl', click.WebsiteDetailCtrl).config([
+}).controller('websiteListCtrl', click.WebsiteListCtrl.prototype.injection()).controller('createWebsiteCtrl', click.CreateWebsiteCtrl).controller('websiteDetailCtrl', click.WebsiteDetailCtrl).config([
     '$routeProvider',
-    function ($routeProvicer) {
-        $routeProvicer.when('/', { templateUrl: 'default.html', controller: click.WebsiteListCtrl }).when('/websites', { templateUrl: 'listWebsites.html', controller: click.WebsiteListCtrl }).when('/websites/:webid', { templateUrl: 'webDetail.html', controller: click.WebsiteDetailCtrl }).when('/websites/create', { templateUrl: 'createwebsite.html', controller: click.WebsiteDetailCtrl }).otherwise({ redirectTo: '/' });
+    function ($routeProvider) {
+        $routeProvider.when('/', { templateUrl: 'default.html', controller: click.WebsiteListCtrl }).when('/websites', { templateUrl: 'listWebsites.html', controller: click.WebsiteListCtrl }).when('/websites/:webid', { templateUrl: 'webDetail.html', controller: click.WebsiteDetailCtrl }).when('/websites/create', { templateUrl: 'createwebsite.html', controller: click.WebsiteDetailCtrl }).otherwise({ redirectTo: '/' });
     }
 ]);
 //# sourceMappingURL=app.js.map
--- Test/app.ts
+++ Test/app.ts
@@ -5,11 +5,11 @@
 
 var app = angular.module("click", []) ;
 app.service("data", () => new click.ClientDataService())
-    .controller('createWebsiteCtrl', click.CreateWebsiteCtrl)
     .controller('websiteListCtrl', click.WebsiteListCtrl.prototype.injection())
+    .controller('createWebsiteCtrl', click.CreateWebsiteCtrl)
     .controller('websiteDetailCtrl', click.WebsiteDetailCtrl)
-    .config(['$routeProvider', function($routeProvicer) {
-        $routeProvicer
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
             .when('/', { templateUrl: 'default.html', controller: click.WebsiteListCtrl })
             .when('/websites', { templateUrl: 'listWebsites.html', controller: click.WebsiteListCtrl })
             .when('/websites/:webid', { templateUrl: 'webDetail.html', controller: click.WebsiteDetailCtrl })
